71082fbee88b3408a24d6939289bb94502f84349
Vighnesh <vighnesh@gmail.com>
2026-04-03 23:35:31 +0530

    fix port for deployment

--- server.js
+++ server.js
@@ -113,6 +113,8 @@ app.get("/stats/:shortId", async (req, res) => {
 
 
 //START SERVER
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
